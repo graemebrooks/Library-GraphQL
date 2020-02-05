666aa230be63675ad77f8775ba4e551e15504b58
graemebrooks <graemebrooks@gmail.com>
2020-02-05 15:25:44 -0600

    adds create book and author functionality

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+
 require('dotenv').config();
 require('./config/database');
+
 const app = express();
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}));
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+);
+
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000!');
+	console.log('now listening for requests on port 4000!');
 });
--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.DATABASE_URL, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useUnifiedTopology: true
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
-
 // database connection event
 try {
-	mongoose.connection.on('connected', function() {
-		console.log(`Mongoose connected to: Library Database`);
-	});
-} catch (error) {
-	console.error(error);
+    mongoose.connection.on('connected', function () {
+        console.log(`Mongoose connected to: Library Database`);
+    });
 }
-
+catch (error) {
+    console.error(error);
+}
 module.exports = mongoose;
