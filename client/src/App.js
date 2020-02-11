7b4351f5cc89a3024b6f0c670fea78329305b7a9
graemebrooks <graemebrooks@gmail.com>
2020-02-11 02:01:22 -0600

    implements graphql and apollo on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+
+// Components
+import BookList from './components/BookList';
+
+// Apollo Client Setup
+const client = new ApolloClient({
+	uri: 'http://localhost:4000/graphql'
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<div id="Main">
+				<h1>Reading List</h1>
+				<BookList />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const cors = require('cors');
 require('dotenv').config();
 require('./config/database');
 const app = express();
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}));
+
+// Allow cross-origin requests
+app.use(cors());
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema,
+		graphiql: true
+	})
+);
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000!');
+	console.log('now listening for requests on port 4000!');
 });
